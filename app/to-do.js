0b2c582ad46f8fe913bdcbdc4721fda292438d03
telpuhova <telpuhova@gmail.com>
2018-02-19 11:49:47 -0800

    add a little bit of functionality

--- app/to-do.js
+++ app/to-do.js
@@ -4,8 +4,18 @@ var Task = /** @class */ (function () {
         this.priority = priority;
         this.done = false;
     }
+    Task.prototype.markDone = function () {
+        this.done = true;
+    };
     return Task;
 }());
 var tasks = [];
 tasks.push(new Task('do the dishes', 'medium'));
-console.log(tasks);
+tasks.push(new Task('do laundry', 'high'));
+tasks.push(new Task('buy groceries', 'low'));
+// console.log(tasks);
+tasks[0].markDone();
+for (var _i = 0, tasks_1 = tasks; _i < tasks_1.length; _i++) {
+    var task = tasks_1[_i];
+    console.log(task);
+}
--- app/to-do.ts
+++ app/to-do.ts
@@ -1,10 +1,21 @@
 class Task {
   done: boolean = false;
 
-  constructor(public description: string, public priority: string) {
+  constructor(public description: string, public priority: string) {}
+
+  markDone() {
+    this.done = true;
   }
 }
 
 var tasks: Task[] = [];
 tasks.push(new Task('do the dishes', 'medium'));
-console.log(tasks);
+tasks.push(new Task('do laundry', 'high'));
+tasks.push(new Task('buy groceries', 'low'));
+// console.log(tasks);
+
+tasks[0].markDone();
+
+for (var task of tasks) {
+  console.log(task);
+}
